b9520212aa037da5344117779817337a1ad918f0
Vegard Storheil Eriksen <zyp@jvnv.net>
2010-11-18 01:13:18 +0100

    Prepare for 0.2.1 release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 
 setup(
 	name = 'pyanidb',
-	version = '0.2.0',
+	version = '0.2.1',
 	author = 'Vegard Storheil Eriksen',
 	author_email = 'zyp@jvnv.net',
 	url = 'http://redmine.jvnv.net/projects/pyanidb/',
